e87e73323c79fc4f65c47e56bf8f435c90a74adf
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-12-10 16:11:15 -0300

    real world ocaml (chapter 18: async): let syntax

--- real_world_ocaml_chapter_18_async/main.ml
+++ real_world_ocaml_chapter_18_async/main.ml
@@ -17,7 +17,7 @@ let uppercase_file_bind_operator (filename : string) : unit Deferred.t =
   Reader.file_contents filename >>= fun contents ->
   Writer.save filename ~contents:(String.uppercase contents)
 
-let count_lines (filename : string) : int Deferred.t =
+let count_lines_return (filename : string) : int Deferred.t =
   Reader.file_contents filename >>= fun contents ->
   (*
     return wraps 'a in a Deferred.t:
@@ -28,8 +28,16 @@ let count_lines (filename : string) : int Deferred.t =
   *)
   String.split contents ~on:'\n' |> List.length |> return
 
-let count_lines (filename : string) : int Deferred.t =
+let count_lines_map (filename : string) : int Deferred.t =
   Reader.file_contents filename (* >>| is Deferred.map *) >>| fun contents ->
+  String.split contents ~on:'\n' |> List.length
+
+let count_lines_let_bind (filename : string) : int Deferred.t =
+  let%bind contents = Reader.file_contents filename in
+  String.split contents ~on:'\n' |> List.length |> return
+
+let count_lines_let_map (filename : string) : int Deferred.t =
+  let%map contents = Reader.file_contents filename in
   String.split contents ~on:'\n' |> List.length
 
 let%test_unit "debug" = blocking_file_ops_example ()
