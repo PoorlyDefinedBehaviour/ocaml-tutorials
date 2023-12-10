0a01f9a9d8e9dac0ff10d80388f2b2191748728b
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-12-10 16:24:08 -0300

    real world ocaml (chapter 18: async): ivar / upon / after

--- real_world_ocaml_chapter_18_async/main.ml
+++ real_world_ocaml_chapter_18_async/main.ml
@@ -40,4 +40,38 @@ let count_lines_let_map (filename : string) : int Deferred.t =
   let%map contents = Reader.file_contents filename in
   String.split contents ~on:'\n' |> List.length
 
+let ivar_intro () =
+  let ivar : string Ivar.t = Ivar.create () in
+  let deferred = Ivar.read ivar in
+  (* None *)
+  let _ = Deferred.peek deferred in
+  Ivar.fill ivar "hello";
+  (* Some "hello" *)
+  let _ = Deferred.peek deferred in
+  ()
+
+module type Delayer_intf = sig
+  type t
+
+  val create : Time.Span.t -> t
+  val schedule : t -> (unit -> 'a Deferred.t) -> 'a Deferred.t
+end
+
+module Delayer : Delayer_intf = struct
+  type t = { delay : Time.Span.t; jobs : (unit -> unit) Queue.t }
+
+  let create (delay : Time.Span.t) : t = { delay; jobs = Queue.create () }
+
+  let schedule (delayer : t) (thunk : unit -> 'a Deferred.t) : 'a Deferred.t =
+    let ivar = Ivar.create () in
+    Queue.enqueue delayer.jobs (fun () ->
+        upon (thunk ()) (fun x -> Ivar.fill ivar x));
+
+    upon (after delayer.delay) (fun () ->
+        let job = Queue.dequeue_exn delayer.jobs in
+        job ());
+
+    Ivar.read ivar
+end
+
 let%test_unit "debug" = blocking_file_ops_example ()
